86bc1c46e1e6699978e416aeea7a3c77f55e75fe
Michael Fulghum <micryfulg@gmail.com>
2023-09-02 21:41:15 -0500

    get simple homepage working

--- furfolio/views.py
+++ furfolio/views.py
@@ -1,3 +1,12 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
+from django.views import generic
+from . import models
 
-# Create your views here.
+class Home(generic.ListView):
+    model = models.Offer
+    
+    def get_queryset(self) -> QuerySet[Any]:
+        return models.Offer.objects.all().order_by("-created date")
+    

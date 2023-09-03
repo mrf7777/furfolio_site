9ad3129a9fb0d1714394c9a91a5228d347e15901
Michael Fulghum <micryfulg@gmail.com>
2023-09-02 21:04:47 -0500

    add cutoff date to offer model

--- furfolio/models.py
+++ furfolio/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
-
+from datetime import datetime
 class User(AbstractUser):
     pass
 
 class Offer(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     name = models.CharField(max_length=80, name="name")
+    cutoff_date = models.DateTimeField(name="cutoff date", default=datetime.now)
     created_date = models.DateTimeField(name="created date", auto_now_add=True)
     updated_date = models.DateTimeField(name="updated date", auto_now=True)
     

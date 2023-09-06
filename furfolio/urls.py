e8811144ede24326136a7303bce630b38bee45c3
mrf7777 <micryfulg@gmail.com>
2023-09-06 13:19:53 -0500

    add home link

--- furfolio/urls.py
+++ furfolio/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.Home.as_view(), name="offer_list"),
+    path('', views.Home.as_view(), name="home"),
     path('offers/<pk>/', views.Offer.as_view(), name="offer_detail"),
     path('accounts/signup/', views.SignUp.as_view(), name="signup"),
 ]

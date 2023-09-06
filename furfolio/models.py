aa05c2c16a44cd8c3276bd27e47b36aad7a78c7c
mrf7777 <micryfulg@gmail.com>
2023-09-06 16:57:03 -0500

    whitespace in models

--- furfolio/models.py
+++ furfolio/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from datetime import datetime
+
 class User(AbstractUser):
     pass
 

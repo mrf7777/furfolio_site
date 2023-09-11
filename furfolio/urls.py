d25da8af618e27dfe7b920008cb23a77160e9be9
mrf7777 <micryfulg@gmail.com>
2023-09-11 13:44:55 -0500

    move account urls to furfolio app

--- furfolio/urls.py
+++ furfolio/urls.py
@@ -1,13 +1,26 @@
 from django.urls import path, include
 from . import views
+from django.contrib.auth import views as auth_views
+from furfolio import forms as furfolio_forms
 
 urlpatterns = [
     path('', views.Home.as_view(), name="home"),
+    # offers
     path('offers/create/', views.CreateOffer.as_view(), name="create_offer"),
     path('offers/<pk>/update/', views.UpdateOffer.as_view(), name="update_offer"),
     path('offers/<pk>/delete/', views.DeleteOffer.as_view(), name="delete_offer"),
     path('offers/<pk>/', views.Offer.as_view(), name="offer_detail"),
-    path('accounts/signup/', views.SignUp.as_view(), name="signup"),
+    # users
     path('users/<username>/', views.User.as_view(), name="user"),
     path('users/', views.UserList.as_view(), name="user-list"),
+    # accounts
+    path('accounts/signup/', views.SignUp.as_view(), name="signup"),
+    path(
+        'accounts/login/',
+        auth_views.LoginView.as_view(
+            authentication_form=furfolio_forms.LoginForm
+        ),
+        name="login"
+    ),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name="logout"),
 ]
--- furfolio_site/urls.py
+++ furfolio_site/urls.py
@@ -16,19 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth import views as auth_views
-from furfolio import forms as furfolio_forms
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('furfolio.urls')),
-    # path('accounts/', include("django.contrib.auth.urls")),
-    path(
-        'accounts/login/',
-        auth_views.LoginView.as_view(
-            authentication_form=furfolio_forms.LoginForm
-        ),
-        name="login"
-    ),
-    path('accounts/logout/', auth_views.LogoutView.as_view(), name="logout"),
 ]

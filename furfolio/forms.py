43e0aad1deba48502c06fd6f4201b6a0ebaf54d9
mrf7777 <micryfulg@gmail.com>
2023-09-11 13:34:31 -0500

    style login form

--- furfolio/forms.py
+++ furfolio/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.forms import EmailField
 from django.core.exceptions import ValidationError
 from django import forms
@@ -26,6 +26,13 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = User
         fields = UserCreationForm.Meta.fields + ("email",)
+
+
+class LoginForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["username"].widget.attrs.update({"class": "form-control"})
+        self.fields["password"].widget.attrs.update({"class": "form-control"})
 
 
 class OfferForm(forms.ModelForm):

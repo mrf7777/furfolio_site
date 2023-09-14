d8c8325b59e0d22c77082cbc96fee7912214c47f
Michael Fulghum <micryfulg@gmail.com>
2023-09-14 16:45:55 -0500

    use better media root for deployment

--- furfolio_site/settings.py
+++ furfolio_site/settings.py
@@ -173,7 +173,10 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = '.media/'
+if os.getenv("DEBUG", "False") == "True":
+    MEDIA_ROOT = './media'
+else:
+    MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

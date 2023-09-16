4cb14417535bf9a6a53c0d9e9b0ed353800d8032
Michael Fulghum <micryfulg@gmail.com>
2023-09-15 19:10:26 -0500

    add a privacy policy

--- furfolio/urls.py
+++ furfolio/urls.py
@@ -28,5 +28,6 @@ urlpatterns = [
     # dashboard
     path('dashboard/', views.Dashboard.as_view(), name="dashboard"),
     # static pages
-    path('legal/terms-of-service', pages.TermsOfService.as_view(), name="terms_of_service")
+    path('legal/terms-of-service', pages.TermsOfService.as_view(), name="terms_of_service"),
+    path('legal/privacy-policy', pages.PrivacyPolicy.as_view(), name="privacy_policy")
 ]
--- furfolio/views/pages.py
+++ furfolio/views/pages.py
@@ -3,3 +3,6 @@ from django.views import generic
 
 class TermsOfService(generic.TemplateView):
     template_name = "furfolio/terms_of_service.html"
+    
+class PrivacyPolicy(generic.TemplateView):
+    template_name = "furfolio/privacy_policy.html"

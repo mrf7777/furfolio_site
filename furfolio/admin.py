228c79de02c96aaca2e3dcc55d95df58651d97b9
Michael Fulghum <micryfulg@gmail.com>
2023-09-22 16:38:56 -0500

    add commission model to admin site

--- furfolio/admin.py
+++ furfolio/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import User, Offer
+from .models import User, Offer, Commission
 
 admin.site.register(User)
 admin.site.register(Offer)
+admin.site.register(Commission)

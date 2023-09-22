7e922a00365026b31a52b05d7221b3c0c5691254
Michael Fulghum <micryfulg@gmail.com>
2023-09-22 13:13:42 -0500

    move ToS and privacy policy to pages dir

--- furfolio/views/pages.py
+++ furfolio/views/pages.py
@@ -2,7 +2,7 @@
 from django.views import generic
 
 class TermsOfService(generic.TemplateView):
-    template_name = "furfolio/terms_of_service.html"
+    template_name = "furfolio/pages/terms_of_service.html"
     
 class PrivacyPolicy(generic.TemplateView):
-    template_name = "furfolio/privacy_policy.html"
+    template_name = "furfolio/pages/privacy_policy.html"

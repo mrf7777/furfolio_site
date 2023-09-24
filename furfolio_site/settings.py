86be0ff0eeb637eca2a377b075c3dd93048081d7
Michael Fulghum <micryfulg@gmail.com>
2023-09-23 22:01:57 -0500

    proof of concept of overriding widget templates

--- furfolio_site/settings.py
+++ furfolio_site/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'furfolio_site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "furfolio/templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

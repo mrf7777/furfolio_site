6f8f45f15ec517a8f9b6b448282a209e3082b23e
mrf7777 <micryfulg@gmail.com>
2023-09-25 16:56:33 -0500

    show buyer commissions in buyer dashboard

--- furfolio/views/views.py
+++ furfolio/views/views.py
@@ -38,6 +38,13 @@ class CreatorDashboard(LoginRequiredMixin, generic.TemplateView):
 class BuyerDashboard(LoginRequiredMixin, generic.TemplateView):
     template_name = "furfolio/dashboards/buyer.html"
 
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context["commissions_as_commissioner"] = models.Commission.objects.filter(
+            commissioner=self.request.user.pk,
+        )
+        return context
+
 
 class OfferList(generic.ListView):
     model = models.Offer

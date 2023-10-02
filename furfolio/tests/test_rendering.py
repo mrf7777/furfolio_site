ba353c852071974a4fe2906df7c6897ca9ad37ec
mrf7777 <micryfulg@gmail.com>
2023-10-02 14:43:21 -0500

    test creator dashboard view

--- furfolio/tests/test_rendering.py
+++ furfolio/tests/test_rendering.py
@@ -102,3 +102,41 @@ class UsersSignedInTestCase(TestCase):
     def test_user_list(self):
         response = self.client.get(reverse("user_list"), follow=True)
         self.assertContains(response, self.user.username)
+
+
+class CreatorDashboardTestCase(TestCase):
+    def setUp(self) -> None:
+        self.client = Client()
+        self.user_creator = models.User(
+            username="creator",
+            password="admin",
+            role=models.User.ROLE_CREATOR,
+        )
+        self.user_creator.full_clean()
+        self.user_creator.save()
+        self.user_buyer = models.User(
+            username="buyer",
+            password="admin",
+            role=models.User.ROLE_BUYER,
+        )
+        self.user_buyer.full_clean()
+        self.user_buyer.save()
+        self.offer = models.Offer(
+            author=self.user_creator,
+            name="Offer",
+            description="This is an offer created by a user.",
+        )
+        self.offer.full_clean()
+        self.offer.save()
+        self.commission = models.Commission(
+            commissioner=self.user_buyer,
+            offer=self.offer,
+            initial_request_text="I want that offer.",
+        )
+        self.commission.full_clean()
+        self.commission.save()
+        self.client.force_login(self.user_creator)
+
+    def test_creator_dashboard(self):
+        response = self.client.get(reverse("dashboard"), follow=True)
+        self.assertContains(response, self.commission.initial_request_text)

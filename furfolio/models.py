971e0ebd9acd3bc599e1b5e781026d8d40b0a2b1
mrf7777 <micryfulg@gmail.com>
2023-10-04 14:25:50 -0500

    fix avatar image processing

--- furfolio/models.py
+++ furfolio/models.py
@@ -21,6 +21,25 @@ PIL.ImageFile.LOAD_TRUNCATED_IMAGES = True
 AVERAGE_CHARACTERS_PER_WORD = 4.7
 
 
+def remove_transparency(im, bg_colour=(255, 255, 255)):
+
+    # Only process if image has transparency (http://stackoverflow.com/a/1963146)
+    if im.mode in ('RGBA', 'LA') or (im.mode == 'P' and 'transparency' in im.info):
+
+        # Need to convert to RGBA if LA format due to a bug in PIL (http://stackoverflow.com/a/1963146)
+        alpha = im.convert('RGBA').split()[-1]
+
+        # Create a new background image of our matt color.
+        # Must be RGBA because paste requires both images have the same format
+        # (http://stackoverflow.com/a/8720632  and  http://stackoverflow.com/a/9459208)
+        bg = Image.new("RGBA", im.size, bg_colour + (255,))
+        bg.paste(im, mask=alpha)
+        return bg
+
+    else:
+        return im
+
+
 class User(AbstractUser):
     ROLE_BUYER = "BUYER"
     ROLE_CREATOR = "CREATOR"
@@ -54,17 +73,19 @@ class User(AbstractUser):
             image = Image.open(self.avatar.path)
             if (image.width, image.height) != User.AVATAR_SIZE_PIXELS:
                 image.thumbnail(User.AVATAR_SIZE_PIXELS,
-                                resample=Image.LANCZOS, reducing_gap=3.0)
+                                resample=Image.BICUBIC, reducing_gap=10)
                 new_image = Image.new(
-                    "RGB",
-                    User.AVATAR_SIZE_PIXELS
+                    "RGBA",
+                    User.AVATAR_SIZE_PIXELS,
+                    (255, 255, 255)
                 )
                 new_image.paste(
                     image,
                     ((User.AVATAR_SIZE_PIXELS[0] - image.size[0]) // 2,
                      (User.AVATAR_SIZE_PIXELS[1] - image.size[1]) // 2),
                 )
-                new_image.save(self.avatar.path, format="PNG")
+                new_image = remove_transparency(new_image)
+                new_image.save(self.avatar.path, format="PNG", optimize=True)
 
     def full_text_search_creators(text_query: str):
         text_query_cleaned = text_query.strip()

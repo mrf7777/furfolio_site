126f29613a93a9c7732034d6a0d5af2126157a8a
Michael Fulghum <micryfulg@gmail.com>
2023-10-06 18:27:08 -0500

    extract email sending

--- furfolio/email.py
+++ furfolio/email.py
@@ -1,22 +1,44 @@
+from typing import Any
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 from . import models
 
 
-def send_commission_state_changed_email(new_commission: 'models.Commission'):
+def send_email(
+    text_template_path: str, 
+    html_template_path: str,
+    context: dict[str, Any],
+    subject: str,
+    from_address: str,
+    to_address: str,
+    reply_to: str = None,
+    ) -> int:
     text_content = render_to_string(
-        "furfolio/email/commissions/commission_state_changed.txt",
-        context={"commission": new_commission}
+        text_template_path,
+        context=context,        
     )
     html_content = render_to_string(
-        "furfolio/email/commissions/commission_state_changed.html",
-        context={"commission": new_commission}
+        html_template_path,
+        context=context,
     )
     email = EmailMultiAlternatives(
-        "Commission state changed",
+        subject,
         text_content,
-        "admin@furfolio.net",
-        [new_commission.commissioner.email]
+        from_address,
+        [to_address,],
+        reply_to=[reply_to,],
     )
     email.attach_alternative(html_content, "text/html")
-    email.send(fail_silently=False)
+    return email.send()
+
+
+def send_commission_state_changed_email(new_commission: 'models.Commission'):
+    send_email(
+        "furfolio/email/commissions/commission_state_changed.txt",
+        "furfolio/email/commissions/commission_state_changed.html",
+        {"commission": new_commission},
+        "Commission state changed",
+        "admin@furfolio.net",
+        new_commission.commissioner.email,
+        "no-reply@furfolio.net",
+    )

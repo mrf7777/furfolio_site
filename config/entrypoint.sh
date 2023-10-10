c568335235d389a72ee8a42a235b8739cbd94fac
Michael Fulghum <micryfulg@gmail.com>
2023-10-09 19:13:23 -0500

    specify tmp dir for gunicorn in entrypoint

--- config/entrypoint.sh
+++ config/entrypoint.sh
@@ -2,6 +2,6 @@
 RUN_PORT="8000"
 
 /opt/venv/bin/python manage.py migrate --no-input
-/opt/venv/bin/gunicorn furfolio_site.wsgi --bind "0.0.0.0:${RUN_PORT}" --daemon
+/opt/venv/bin/gunicorn furfolio_site.wsgi --worker-tmp-dir /dev/shm --bind "0.0.0.0:${RUN_PORT}" --daemon
 
 nginx -g 'daemon off;'

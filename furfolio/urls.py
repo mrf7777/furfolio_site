f9fa80cd4422060eaf35e581cce2e68efeae0cc3
Michael Fulghum <micryfulg@gmail.com>
2023-10-13 19:35:34 -0500

    implement password reset

--- furfolio/urls.py
+++ furfolio/urls.py
@@ -27,6 +27,10 @@ urlpatterns = [
         name="login"
     ),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name="logout"),
+    path('accounts/password_reset', auth_views.PasswordResetView.as_view(), name="password_reset"),
+    path('accounts/password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
+    path('accounts/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path('accounts/reset/done/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
     # dashboards
     path('dashboard/', views.DashboardRedirector.as_view(), name="dashboard"),
     path('dashboard/creator/', views.CreatorDashboard.as_view(),

918320409a75e10e4154d6dd46bd2d853d0ecc0e
mrf7777 <micryfulg@gmail.com>
2023-11-03 16:18:57 -0500

    bundle all javascript with bootstrap

--- dev-sass.sh
+++ dev-sass.sh
@@ -3,8 +3,8 @@ CUSTOM_BOOTSTRAP_CSS_TARGET_DIRECTORY="./furfolio/static/style"
 CUSTOM_BOOTSTRAP_CSS_TARGET_FILE="${CUSTOM_BOOTSTRAP_CSS_TARGET_DIRECTORY}/style.css"
 # where do you want to place the bootstrap javascript file?
 BOOTSTRAP_JS_TARGET_DIRECTORY="./furfolio/static/js"
-BOOTSTRAP_JS_TARGET_FILE="${BOOTSTRAP_JS_TARGET_DIRECTORY}/bootstrap.min.js"
-BOOTSTRAP_JS_TARGET_MAP_FILE="${BOOTSTRAP_JS_TARGET_DIRECTORY}/bootstrap.min.js.map"
+BOOTSTRAP_JS_TARGET_FILE="${BOOTSTRAP_JS_TARGET_DIRECTORY}/bootstrap.bundle.min.js"
+BOOTSTRAP_JS_TARGET_MAP_FILE="${BOOTSTRAP_JS_TARGET_DIRECTORY}/bootstrap.bundle.min.js.map"
 # where is the bootstrap source?
 BOOTSTRAP_SOURCE_DIR="./bootstrap-5.3.2"
 
@@ -15,6 +15,6 @@ rm -rf ./furfolio/static/js/
 mkdir -p $CUSTOM_BOOTSTRAP_CSS_TARGET_DIRECTORY
 mkdir -p $BOOTSTRAP_JS_TARGET_DIRECTORY
 
-cp $BOOTSTRAP_SOURCE_DIR/dist/js/bootstrap.min.js $BOOTSTRAP_JS_TARGET_FILE
-cp $BOOTSTRAP_SOURCE_DIR/dist/js/bootstrap.min.js.map $BOOTSTRAP_JS_TARGET_MAP_FILE
+cp $BOOTSTRAP_SOURCE_DIR/dist/js/bootstrap.bundle.min.js $BOOTSTRAP_JS_TARGET_FILE
+cp $BOOTSTRAP_SOURCE_DIR/dist/js/bootstrap.bundle.min.js.map $BOOTSTRAP_JS_TARGET_MAP_FILE
 sass --watch ./furfolio/sass/furfolio-theme.scss  $CUSTOM_BOOTSTRAP_CSS_TARGET_FILE

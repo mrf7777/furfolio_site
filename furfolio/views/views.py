4a768fe34fa58a692f94fb952b2cc1c3db5a3ae0
Michael Fulghum <micryfulg@gmail.com>
2023-11-09 20:38:39 -0600

    cleanup config for PageRangeContextMixin

--- furfolio/views/views.py
+++ furfolio/views/views.py
@@ -26,20 +26,22 @@ from ..forms import CommissionSearchForm, CustomUserCreationForm, OfferForm, Com
 PAGE_SIZE = 10
 
 
-def get_page_range_items(page: Page) -> List[str]:
-    """
-    Given the page object, which you should get from the context, this
-    will return a list of page items, including ellipsis, that you
-    can then display in a template.
-    """
-    return list(page.paginator.get_elided_page_range(page.number))
-
-
 class PageRangeContextMixin:
+    page_range_context_object_name = "page_range"
+
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
-        context["page_range"] = get_page_range_items(context["page_obj"])
+        context[self.page_range_context_object_name] = self.__class__.get_page_range_items(
+            context["page_obj"])
         return context
+
+    def get_page_range_items(page: Page) -> List[str]:
+        """
+        Given the page object, which you should get from the context, this
+        will return a list of page items, including ellipsis, that you
+        can then display in a template.
+        """
+        return list(page.paginator.get_elided_page_range(page.number))
 
 
 class Home(generic.TemplateView):

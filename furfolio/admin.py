93061ebc885704a3adecc2660d9fd0408e74ef5d
Michael Fulghum <micryfulg@gmail.com>
2024-01-01 16:08:54 -0600

    add chat models to admin site

--- furfolio/admin.py
+++ furfolio/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import User, Offer, Commission, CommissionMessage, UserFollowingUser, Tag, TagCategory
+from .models import Chat, ChatMessage, ChatParticipant, User, Offer, Commission, CommissionMessage, UserFollowingUser, Tag, TagCategory
 
 admin.site.register(User)
 admin.site.register(Offer)
@@ -9,3 +9,6 @@ admin.site.register(CommissionMessage)
 admin.site.register(UserFollowingUser)
 admin.site.register(Tag)
 admin.site.register(TagCategory)
+admin.site.register(Chat)
+admin.site.register(ChatParticipant)
+admin.site.register(ChatMessage)

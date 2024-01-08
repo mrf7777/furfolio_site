a520a4fdfbe2e3a02a16d98d514551f9b7dbeec0
Michael Fulghum <micryfulg@gmail.com>
2024-01-07 19:42:48 -0600

    test message notification

--- furfolio/tests/test_chat.py
+++ furfolio/tests/test_chat.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 from django.core.exceptions import ValidationError
 from . import utils
 from .. import utils as furfolio_utils
+from .. import models
 from ..queries import commissions as commission_queries
 import datetime
 
@@ -23,3 +24,22 @@ class ChatParticipantCanMessageTestCase(TestCase):
     def test_non_participating_user_cannot_message(self):
         with self.assertRaises(ValidationError):
             utils.make_chat_message(self.chat, self.user3)
+            
+
+class ChatMessageNotificationTestCase(TestCase):
+    def setUp(self):
+        self.user1 = utils.make_user("user1")
+        self.user2 = utils.make_user("user2")
+        self.user3 = utils.make_user("user3")
+        self.user4 = utils.make_user("user4")
+        self.chat = utils.make_chat()
+        for user in [self.user1, self.user2, self.user3]:
+            utils.add_chat_participant(self.chat, user)
+            
+    def test_message_notifies_participants(self):
+        utils.make_chat_message(self.chat, self.user1)
+        self.assertQuerySetEqual(
+            models.ChatMessageNotification.objects.order_by("pk").all(),
+            [self.user2.pk, self.user3.pk],
+            transform=lambda message_noti: message_noti.notification.recipient.pk
+        )

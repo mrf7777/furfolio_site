d6c4fee412291c4d05b3d4ac528b5d70a2fde268
mrf7777 <micryfulg@gmail.com>
2024-01-09 13:28:47 -0600

    remove unused imports in model files

--- furfolio/models/offers.py
+++ furfolio/models/offers.py
@@ -1,18 +1,11 @@
-from PIL import Image
-import PIL.ImageFile
-from io import BytesIO
-from pathlib import Path
 from model_utils import FieldTracker
 from django.db import models
 from django.conf import settings
-from django.contrib.auth.models import AbstractUser
 from django.contrib.postgres.indexes import GinIndex
 from django.core import validators
-from django.core.files import File
 from django.urls import reverse
 from django.utils import timezone
 from django.utils.safestring import mark_safe
-from datetime import timedelta
 import math
 
 from .utils import image_resize, seven_days_from_now
@@ -20,7 +13,6 @@ from .content_rating import RATING_TO_CHOICES, RATING_GENERAL, RATING_ADULT
 from .. import validators as furfolio_validators
 from .. import mixins
 from ..queries import commissions as commission_queries
-from ..queries import users as user_queries
 from ..queries import offers as offer_queries
 from ..queries import notifications as notification_queries
 

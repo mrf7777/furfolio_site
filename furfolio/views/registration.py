c0c5f1ab252b30632698a8947ce3598e1d2c860c
Michael Fulghum <micryfulg@gmail.com>
2024-01-14 16:34:45 -0600

    finish after sign up for creators and buyers

--- furfolio/views/registration.py
+++ furfolio/views/registration.py
@@ -1,12 +1,15 @@
 
+from typing import Any
 from django.forms.models import BaseModelForm
 from django.http import HttpResponse, HttpResponseServerError
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import generic
 from django.contrib import auth
 from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
 from honeypot.decorators import check_honeypot
 from .. import forms
+from .. import models
 
 # decorate the 'post' method of this class to check for honeypot
 
@@ -31,5 +34,11 @@ class SignUp(generic.CreateView):
         return response
 
 
-class AfterSignUp(generic.TemplateView):
+class AfterSignUp(generic.TemplateView, LoginRequiredMixin):
     template_name = "furfolio/pages/after_sign_up.html"
+    
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context["show_creator_next_steps"] = self.request.user.role == models.User.ROLE_CREATOR
+        context["show_buyer_next_steps"] = self.request.user.role == models.User.ROLE_BUYER
+        return context

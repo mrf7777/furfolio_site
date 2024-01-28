bd847c36c6322071d416e22a357317cedc1f4f96
Michael Fulghum <micryfulg@gmail.com>
2024-01-27 21:10:32 -0600

    draft save and clean framework mixins

--- furfolio/mixins.py
+++ furfolio/mixins.py
@@ -14,3 +14,52 @@ class GetAdultConsentMixin:
             self.request.user, "consent_to_adult_content", False
         )
         return consent_to_adult_content
+
+
+class SavingMixin:    
+    def save(self, *args, **kwargs) -> None:
+        # https://docs.djangoproject.com/en/5.0/ref/models/instances/#django.db.models.Model._state
+        if self._state.adding:
+            self.pre_save_new_object()
+            super().save(*args, **kwargs)
+            self.post_save_new_object()
+        else:
+            self.pre_save_existing_object()
+            super().save(*args, **kwargs)
+            self.post_save_existing_object()
+        
+    def pre_save_new_object(self):
+        pass
+    
+    def post_save_new_object(self):
+        pass
+    
+    def pre_save_existing_object(self):
+        pass
+    
+    def post_save_existing_object(self):
+        pass
+    
+    
+class CleaningMixin:
+    def clean(self):
+        if self._state.adding:
+            self.pre_clean_new_object()
+            super().clean()
+            self.post_clean_new_object()
+        else:
+            self.pre_clean_existing_object()
+            super().clean()
+            self.post_clean_existing_object()
+            
+    def pre_clean_new_object(self):
+        pass
+    
+    def post_clean_new_object(self):
+        pass
+    
+    def pre_clean_existing_object(self):
+        pass
+    
+    def post_clean_existing_object(self):
+        pass

7099c01f112348ac45a78541f10bcbd4a7bfec21
mrf7777 <micryfulg@gmail.com>
2024-01-31 11:47:32 -0600

    add different chats to admin

--- furfolio/admin.py
+++ furfolio/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import SupportTicket, Notification, ChatMessageNotification, Chat, ChatMessage, ChatParticipant, User, Offer, Commission, UserFollowingUser, Tag, TagCategory
+from .models import SupportTicket, Notification, ChatMessageNotification, CommissionChat, SupportTicketChat, Chat, ChatMessage, ChatParticipant, User, Offer, Commission, UserFollowingUser, Tag, TagCategory
 
 admin.site.register(User)
 admin.site.register(Offer)
@@ -9,6 +9,8 @@ admin.site.register(UserFollowingUser)
 admin.site.register(Tag)
 admin.site.register(TagCategory)
 admin.site.register(Chat)
+admin.site.register(CommissionChat)
+admin.site.register(SupportTicketChat)
 admin.site.register(ChatParticipant)
 admin.site.register(ChatMessage)
 admin.site.register(Notification)
